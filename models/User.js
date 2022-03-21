64cc38d2cddea5a2e29a8437481f1112d5099346
Cristiana Dragomir <cristiana_dragomir99@yahoo.com>
2022-03-21 19:22:53 +0200

    21.03.2022, 19:22

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: TextTrackCue
+        required: true
     },
     email: {
         type: String,
