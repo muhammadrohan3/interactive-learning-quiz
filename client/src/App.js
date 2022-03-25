448a8fd5e66d4e67cefa434045a54a465b2d1b14
Cristiana Dragomir <cristiana_dragomir99@yahoo.com>
2022-03-25 15:17:57 +0200

    Add 'Passwords do not match' alert & remove after 5secs

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -15,14 +16,15 @@ const App = () => (
     <Router>
       <Fragment>
         <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+        </Routes>
+        <section className='container'>
+          <Alert></Alert>
           <Routes>
-            <Route exact path='/' element ={<Landing />} />
+            <Route exact path='/register' element={<Register />} />
+            <Route exact path='/login' element={<Login />} />
           </Routes>
-          <section className="container">
-            <Routes>
-              <Route exact path='/register' element={<Register />} />
-              <Route exact path='/login' element={<Login />} />
-            </Routes>
         </section>
       </Fragment>
     </Router>
