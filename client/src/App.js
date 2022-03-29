10d6e9d644f544084ec244c322877579c8a6146e
Cristiana Dragomir <cristiana_dragomir99@yahoo.com>
2022-03-29 16:25:44 +0300

    Add Education & Experience

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
@@ -60,6 +62,22 @@ const App = () => {
                 element={
                   <PrivateRoute>
                     <EditProfile />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path='/add-experience'
+                element={
+                  <PrivateRoute>
+                    <AddExperience />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path='/add-education'
+                element={
+                  <PrivateRoute>
+                    <AddEducation />
                   </PrivateRoute>
                 }
               />
