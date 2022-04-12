f233cbe25e94f1330055f6def87b7763b20fe0a7
Cristiana Dragomir <cristiana_dragomir99@yahoo.com>
2022-04-12 20:21:43 +0300

    Final Touches

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,8 @@ import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
+import { LOGOUT } from './actions/types';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -29,7 +29,19 @@ if (localStorage.token) {
 
 const App = () => {
   useEffect(() => {
+    // check for token in LS when app first runs
+    if (localStorage.token) {
+      // if there is a token set axios headers for all requests
+      setAuthToken(localStorage.token);
+    }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
     store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
   }, []);
 
   return (
@@ -92,14 +104,6 @@ const App = () => {
                 element={
                   <PrivateRoute>
                     <Posts />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path='/posts/:id'
-                element={
-                  <PrivateRoute>
-                    <Post />
                   </PrivateRoute>
                 }
               />
